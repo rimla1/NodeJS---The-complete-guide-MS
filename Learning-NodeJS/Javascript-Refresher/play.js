9c9da588451e3ba8556da283f0d0babdb1c20079
Almir Muminovic <projectalmir@gmail.com>
2022-02-04 12:06:57 +0100

    Creating simple object

--- Learning-NodeJS/Javascript-Refresher/play.js
+++ Learning-NodeJS/Javascript-Refresher/play.js
@@ -1,12 +1,5 @@
-const myName = 'Almir';
-let age = 19;
-const hasHobbies = true
+const person = {
+    name: 'Almir',
+    age: 19
+};
 
-age = 20
-
-const bioOfUser = (nameOfUser, ageOfUser, hasHobbiesUser) => {
-    return ('My name is: ' + nameOfUser + ', my age: ' + ageOfUser + ' and have hobby is: ' + hasHobbiesUser + '.')
-}
-
-
- console.log(bioOfUser(myName, age, hasHobbies))
