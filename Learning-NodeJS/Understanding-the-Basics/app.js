1a8fdf2589bd7aa47971febb3b0af66fa90b5b90
Almir Muminovic <projectalmir@gmail.com>
2022-02-06 18:30:59 +0100

    Routing Requests

--- Learning-NodeJS/Understanding-the-Basics/app.js
+++ Learning-NodeJS/Understanding-the-Basics/app.js
@@ -2,8 +2,14 @@ const http = require("http")
 
 
 const server = http.createServer((req, res) => {
-     console.log(req.headers)
-     // process.exit()
+    const url = req.url;
+    if(url === '/') {
+        res.write('<head><title>Enter message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="messageRandom"><button type="submit">Send</button></form></body>');
+        res.write('<html>');
+        res.write('</html>');
+        return res.end();
+    }
      res.setHeader('Content-Type', 'text/html');
      res.write('<head><title>My First Page</title></head>');
      res.write('<body><h1>Hello from my Node.js Server</h1></body>');
