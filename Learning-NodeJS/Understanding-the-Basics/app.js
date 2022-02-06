01dcff43659926cba2f54b75ea1484f1ec5ccb25
Almir Muminovic <projectalmir@gmail.com>
2022-02-06 18:47:05 +0100

    Redirecting Requests

--- Learning-NodeJS/Understanding-the-Basics/app.js
+++ Learning-NodeJS/Understanding-the-Basics/app.js
@@ -1,14 +1,21 @@
 const http = require("http")
+const fs = require('fs');
 
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
     if(url === '/') {
         res.write('<head><title>Enter message</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="messageRandom"><button type="submit">Send</button></form></body>');
         res.write('<html>');
         res.write('</html>');
         return res.end();
+    }
+    if(url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'Dummy');
+        res.writeHead(302, {'location': '/'})
+        return res.end()
     }
      res.setHeader('Content-Type', 'text/html');
      res.write('<head><title>My First Page</title></head>');
