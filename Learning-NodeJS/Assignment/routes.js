78958a483c33ecbc1ff138d2b08721e9ce10005a
Almir Muminovic <projectalmir@gmail.com>
2022-02-07 20:03:11 +0100

    Adding /create-user route, parsing data and console.log result

--- Learning-NodeJS/Assignment/routes.js
+++ Learning-NodeJS/Assignment/routes.js
@@ -1,13 +1,15 @@
 const requestHandler = (req, res) => {
     const url = req.url;
+    const method = req.method;
+
     if (url === '/') {
         // Response Header
         res.setHeader('Content-Type', 'text/html')
 
         // Response Content
-        res.write('<html><body><form action="/create-user method="POST"><input type="text" name="userRandom"><button type="submit">submit</button></form></body></html>')
+        res.write('<html><body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">submit</button></form></body></html>')
 
-        res.end();
+        return res.end();
     } 
     
     if (req.url === '/users') {
@@ -17,7 +19,19 @@ const requestHandler = (req, res) => {
         // Response Content
         res.write('<html><body><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul></body></html>')
 
-        res.end();
+        return res.end();
+    }
+
+    if (req.url === '/create-user' && req.method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk)
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody.split('=')[1])
+        })
+        return res.end();
     }
 
 }
