678ff260184ff919e6bcf2cc3dc3cf94e70224da
Almir Muminovic <projectalmir@gmail.com>
2022-02-07 14:37:19 +0100

    Using the Node Modules System for better structure of files

--- Learning-NodeJS/Understanding-the-Basics/app.js
+++ Learning-NodeJS/Understanding-the-Basics/app.js
@@ -1,40 +1,8 @@
 const http = require("http")
-const fs = require('fs');
 
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if(url === '/') {
-        res.write('<head><title>Enter message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="messageRandom"><button type="submit">Send</button></form></body>');
-        res.write('<html>');
-        res.write('</html>');
-        return res.end();
-    }
-    if(url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk)
-            body.push(chunk);
-        })
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const messageData = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', messageData, (err) => {
-            res.writeHead(302, {'location': '/'})
-            return res.end()
-            });
+const server = http.createServer(routes.handler);
 
-        })
-
-    }
-     res.setHeader('Content-Type', 'text/html');
-     res.write('<head><title>My First Page</title></head>');
-     res.write('<body><h1>Hello from my Node.js Server</h1></body>');
-     res.write('<html>');
-     res.write('</html>');
-    res.end();
-});
 
 server.listen(3000);
