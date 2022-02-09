f368513712fc79e4be9e81e13822c9569567b1bc
Almir Muminovic <projectalmir@gmail.com>
2022-02-09 13:11:36 +0100

    Fixing runtime error by returning the res.end() at routes.js:line:12

--- Learning-NodeJS/Understanding-the-Basics/routes.js
+++ Learning-NodeJS/Understanding-the-Basics/routes.js
@@ -9,7 +9,7 @@ const requestHandler = (req, res) => {
         res.write('<body><form action="/message" method="POST"><input type="text" name="messageRandom"><button type="submit">Send</button></form></body>');
         res.write('<html>');
         res.write('</html>');
-        res.end();
+        return res.end();
     }
     if(url === '/message' && method === 'POST') {
         const body = [];
