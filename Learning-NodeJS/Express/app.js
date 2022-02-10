682db327938899366518eba54fdce8173653e145
Almir Muminovic <projectalmir@gmail.com>
2022-02-10 23:16:07 +0100

    Created middleware to funnel request

--- Learning-NodeJS/Express/app.js
+++ Learning-NodeJS/Express/app.js
@@ -4,6 +4,11 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('in the middleware')
+})
+
+
 const server = http.createServer(app);
 
 server.listen(3210)
