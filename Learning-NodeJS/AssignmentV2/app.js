aacf99487d3fe780af5286bd91ae760bcd02bf0a
Almir Muminovic <projectalmir@gmail.com>
2022-02-11 16:50:22 +0100

    Final update

--- Learning-NodeJS/AssignmentV2/app.js
+++ Learning-NodeJS/AssignmentV2/app.js
@@ -4,10 +4,12 @@ const app = express();
 
 app.use('/users', (req, res, next) => {
     res.send('<h1><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul></h1>')
+    console.log('middleware 1')
 })
 
 app.use('/', (req, res, next) => {
     res.send('<h1>Welcome to Home Page!</h1>')
+    console.log('middleware 2')
 })
 
 app.listen(3333);
