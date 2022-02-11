4ce595324e878a8fd2341752e4bf50bc8bfa5510
Almir Muminovic <projectalmir@gmail.com>
2022-02-11 15:24:36 +0100

    Adding two middlewares functions to funnel request

--- Learning-NodeJS/AssignmentV2/app.js
+++ Learning-NodeJS/AssignmentV2/app.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('Middleware number 1')
+    next();
+})
 
+app.use((req, res, next) => {
+    console.log('Middleware number 2')
+    res.send('<h1>Welcome!</h1>')
+})
 
 app.listen(3333);
