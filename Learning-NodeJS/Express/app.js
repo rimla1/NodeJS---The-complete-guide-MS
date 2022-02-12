3a1fb582312df66c28f513945cca6a6b90c33bff
Almir Muminovic <projectalmir@gmail.com>
2022-02-13 00:22:14 +0100

    Adding error page (Maximilian's way)

--- Learning-NodeJS/Express/app.js
+++ Learning-NodeJS/Express/app.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
-const errorRoutes = require('./routes/error')
 
 const app = express();
 
@@ -15,7 +14,10 @@ app.use(adminRoutes)
 
 app.use(shopRoutes)
 
-app.use(errorRoutes)
+// catch all middlewares (path is / by default)
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Error 404</h1>')
+})
 
 
 
