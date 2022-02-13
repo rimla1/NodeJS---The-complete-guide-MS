bd820c7cf41ad4ac26d3485acd75ca48b47fd563
Almir Muminovic <projectalmir@gmail.com>
2022-02-13 13:54:37 +0100

    Using helper function for navigation in shop.js and admin.js

--- Learning-NodeJS/Express/routes/shop.js
+++ Learning-NodeJS/Express/routes/shop.js
@@ -2,11 +2,13 @@ const path = require('path')
 
 const express = require('express');
 
+const rootDir = require('../util/path')
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
     //res.send('<h1>Home page with path "/"</h1>');
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 })
 
 module.exports = router
