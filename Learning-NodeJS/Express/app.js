cebeeb467e77be86c81cbb622bb1d3672469027b
Almir Muminovic <projectalmir@gmail.com>
2022-02-13 12:36:59 +0100

    Serving HTML page

--- Learning-NodeJS/Express/app.js
+++ Learning-NodeJS/Express/app.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -17,7 +19,7 @@ app.use(shopRoutes)
 
 // catch all middlewares (path is / by default)
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Error 404</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'))
 })
 
 
