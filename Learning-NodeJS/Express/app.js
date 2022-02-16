a1d8c1be874c2905b0359644b552061429340786
Almir Muminovic <projectalmir@gmail.com>
2022-02-16 12:49:21 +0100

    Setup for Handlebars Template

--- Learning-NodeJS/Express/app.js
+++ Learning-NodeJS/Express/app.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars')
 
 const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop');
@@ -15,6 +16,10 @@ const app = express();
 // app.set('view engine', 'pug')
 // app.set('views', 'views')
 
+// Handlebars Template
+app.engine('handlebars', expressHbs());
+app.set('view engine', 'handlebars')
+app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
