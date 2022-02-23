1ba962ccabfb6aa8e91bba04429a43270de44ff8
Almir Muminovic <projectalmir@gmail.com>
2022-02-23 12:24:29 +0100

    Adding another item for holding users orders

--- Learning-NodeJS/Express/controllers/shop.js
+++ Learning-NodeJS/Express/controllers/shop.js
@@ -19,3 +19,7 @@ exports.getCart = (req, res, next) => {
 exports.getCheckout = (req, res, next) => {
     res.render('shop/checkout', {path: '/checkout', pageTitle: 'Checkout Page'})
 }
+
+exports.getOrders = (req, res, next) => {
+    res.render('shop/orders', {path: '/orders', pageTitle: 'Orders'})
+}
