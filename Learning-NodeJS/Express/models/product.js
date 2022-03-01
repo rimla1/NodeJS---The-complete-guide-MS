d9667aeb1207abe036fbd56d1bfa5fae0bc12b8d
Almir Muminovic <projectalmir@gmail.com>
2022-03-01 21:58:38 +0100

    Deleting Cart Items

--- Learning-NodeJS/Express/models/product.js
+++ Learning-NodeJS/Express/models/product.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require ('path');
 const rootDir = require('../util/pathRoot')
 
+const Cart = require('./cart')
+
 const p = path.join(rootDir, 'data', 'products.json');
 
 const getProductsFromFile = (cb) => {
@@ -45,10 +47,11 @@ module.exports = class Product {
 
     static deleteById(id) {
         getProductsFromFile(products => {
+            const product = products.find(prod => prod.id === id)
             const updatedProducts = products.filter(prod => prod.id !== id);
             fs.writeFile(p, JSON.stringify(updatedProducts), err => {
                 if (!err) {
-                    
+                    Cart.deleteProduct(id, product.price);
                 }
             });
         })
