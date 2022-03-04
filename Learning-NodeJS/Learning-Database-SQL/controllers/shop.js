6f32a3aae6f7662be7c25b832428d1b9301275cb
Almir Muminovic <projectalmir@gmail.com>
2022-03-04 15:36:43 +0100

    Fetching a single product with the WHERE condition

--- Learning-NodeJS/Learning-Database-SQL/controllers/shop.js
+++ Learning-NodeJS/Learning-Database-SQL/controllers/shop.js
@@ -14,13 +14,18 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getProduct = (req, res, next) => {
-  const prodId = req.params.productId;
-  Product.findById(prodId, product => {
+  const prodId = req.params.productId
+  Product.findById(prodId)
+  .then(([product]) => {
+    console.log(product)
     res.render('shop/product-detail', {
-      product: product,
+      product: product[0],
       pageTitle: product.title,
       path: '/products'
     });
+  })
+  .catch((err) => {
+    console.log(err)
   });
 };
 
