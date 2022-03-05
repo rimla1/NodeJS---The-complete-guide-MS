7bfa1dc78500924ec5a98cd338cc98673a2d616e
Almir Muminovic <projectalmir@gmail.com>
2022-03-05 13:37:55 +0100

    Syncing JS definitions to the Database

--- Learning-NodeJS/Learning-Database-SQL/util/database.js
+++ Learning-NodeJS/Learning-Database-SQL/util/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('node-complete', 'root', 'punim11godina', {
     dialect: 'mysql', 
-    host: 'locahost'
+    host: 'localhost'
 })
 
 module.exports = sequelize
