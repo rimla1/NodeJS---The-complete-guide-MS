26d76c57cff761caa6b340f06e807d561ba4ba20
Almir Muminovic <projectalmir@gmail.com>
2022-03-05 13:38:55 +0100

    Commenting console.log

--- Learning-NodeJS/Learning-Database-SQL/app.js
+++ Learning-NodeJS/Learning-Database-SQL/app.js
@@ -28,7 +28,7 @@ app.use(errorController.get404);
 sequelize
     .sync()
     .then(result => {
-        console.log(result)
+        // console.log(result)
         app.listen(3001)
     })
     .catch(err => {
