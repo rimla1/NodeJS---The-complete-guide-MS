a5cff468bb8ead85c37d0ae5888e1bd485378f5c
Almir Muminovic <projectalmir@gmail.com>
2022-03-07 16:09:06 +0100

    Creating & Managing a Dummy User

--- Learning-NodeJS/Learning-Database-SQL/app.js
+++ Learning-NodeJS/Learning-Database-SQL/app.js
@@ -22,6 +22,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => console.log(err => console.log(err)))
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -31,9 +40,20 @@ Product.belongsTo(User, {constrains: true, onDelete: 'CASCADE'})
 User.hasMany(Product)
 
 sequelize
-    .sync({ force: true })
+    // .sync({ force: true }) we use this when we want to overwrite tables in database
+    .sync()
     .then(result => {
+        return User.findByPk(1)
         // console.log(result)
+    })
+    .then(user => {
+        if (!user) {
+            User.create({name: 'Almir', email: 'test@test.com'})
+        } 
+        return user;
+    })
+    .then(user => {
+        console.log(user)
         app.listen(3001)
     })
     .catch(err => {
