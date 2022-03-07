e85d37f5bb87338265abec9a7b7a5a23e820c272
Almir Muminovic <projectalmir@gmail.com>
2022-03-07 16:35:36 +0100

    using await without storing it to the const to see if it works, it works

--- Learning-NodeJS/Learning-Database-SQL/controllers/admin.js
+++ Learning-NodeJS/Learning-Database-SQL/controllers/admin.js
@@ -12,7 +12,7 @@ exports.postAddProduct = async(req, res, next) => {
   // Destructuring (instead of const title = req.body.title & const imageUrl = req.body.imageUrl...) we can do this:
   const {title, imageUrl, price, description} = req.body;
   try {
-    const product = await req.user.createProduct({
+     await req.user.createProduct({
       // title: title (if names matches) then we can just name it title
       title,
       price,
