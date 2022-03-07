b39ebe130b8abe92898eeac5e2c23ffa6f0c5916
Almir Muminovic <projectalmir@gmail.com>
2022-03-07 14:07:02 +0100

    Updating Products

--- Learning-NodeJS/Learning-Database-SQL/controllers/admin.js
+++ Learning-NodeJS/Learning-Database-SQL/controllers/admin.js
@@ -31,7 +31,7 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
+  Product.findByPk(prodId).then(product => {
     if (!product) {
       return res.redirect('/');
     }
@@ -41,7 +41,7 @@ exports.getEditProduct = (req, res, next) => {
       editing: editMode,
       product: product
     });
-  });
+  }).catch(err => console.log(err))
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -50,15 +50,21 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect('/admin/products');
+  
+  Product.findByPk(prodId)
+  .then(product => {
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.description = updatedDesc;
+    product.imageUrl = updatedImageUrl;
+    // Saves it back to the database
+    return product.save()
+  })
+  .then(result => {
+    console.log('Updated Product!')
+    res.redirect('/admin/products');
+  })
+  .catch(err => console.log(err))
 };
 
 exports.getProducts = (req, res, next) => {
