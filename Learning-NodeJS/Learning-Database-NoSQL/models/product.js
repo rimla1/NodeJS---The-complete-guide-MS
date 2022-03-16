5615f700d03ae2b7d88857c1d14b682314520c02
Almir Muminovic <projectalmir@gmail.com>
2022-03-16 15:07:53 +0100

    Creating Products

--- Learning-NodeJS/Learning-Database-NoSQL/models/product.js
+++ Learning-NodeJS/Learning-Database-NoSQL/models/product.js
@@ -1,23 +1,20 @@
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(t, p, d, img) {
-    this.title = t;
-    this.price = p;
-    this.description = d;
-    this.imageUrl = img;
+  constructor(title, price, description, imageUrl) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
   }
 
   save() {
     // Database connection
     const db = getDb();
-    // db.collection('products').insertOne(this).then(result => {console.log(result)}).catch(err => console.log(err))
-    try {
-      result = db.collection('products').insertOne(this)
-      console.log(result)
-    } catch (err) {
-      console.log(err)
-    }
+    return db.collection('products')
+    .insertOne(this)
+    .then(result => {console.log(result)})
+    .catch(err => console.log(err))
   }
 }
 
--- Learning-NodeJS/Learning-Database-NoSQL/util/database.js
+++ Learning-NodeJS/Learning-Database-NoSQL/util/database.js
@@ -4,16 +4,29 @@ const MongoClient = mongodb.MongoClient;
 
 let _db;
 
-const mongoConnect = async(callback) => {
-  try {
-    const client = await MongoClient.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.ekxmb.mongodb.net/shop?retryWrites=true&w=majority`)
+// const mongoConnect = async(callback) => {
+//   try {
+//     const client = await MongoClient.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.ekxmb.mongodb.net/shop?retryWrites=true&w=majority`)
+//     console.log('Connection is successful')
+//     _db = client.db()
+//     callback()
+//   } catch (err) {
+//     console.log(err)
+//     throw err;
+//   }
+// }
+
+const mongoConnect = callback => {
+  MongoClient.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.ekxmb.mongodb.net/shop?retryWrites=true&w=majority`)
+  .then(client => {
     console.log('Connection is successful')
     _db = client.db()
     callback()
-  } catch (err) {
+  })
+  .catch(err => {
     console.log(err)
     throw err;
-  }
+  })
 }
 
 const getDb = () => {
@@ -23,16 +36,7 @@ const getDb = () => {
   throw 'No database found!'
 }
 
-// const mongoConnect = callback => {
-//   MongoClient.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.ekxmb.mongodb.net/Cluster0?retryWrites=true&w=majority`)
-//   .then(client => {
-//     console.log('Connection is successful')
-//     callback(client)
-//   })
-//   .catch(err => {
-//     console.log(err)
-//   })
-// }
+
 
 exports.mongoConnect = mongoConnect;
 exports.getDb = getDb
