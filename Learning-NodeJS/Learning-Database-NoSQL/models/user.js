7e296864f473a489cdce9ed34625c816a5e44851
Almir Muminovic <projectalmir@gmail.com>
2022-03-23 19:33:42 +0100

    Working on Cart Items & Orders

--- Learning-NodeJS/Learning-Database-NoSQL/models/user.js
+++ Learning-NodeJS/Learning-Database-NoSQL/models/user.js
@@ -1,38 +1,45 @@
 const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
+const objectId = mongodb.ObjectId;
+
 class User {
-  constructor(username, email, id) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
-    // this._id = id ? new mongodb.ObjectId(id) : null;
+    this.cart = cart;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
-    // .then((result) => {
-    //   console.log(result);
-    // })
-    // .catch((err) => console.log(err));
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex((cp) => {
+    //   return cp._id === product._id;
+    // });
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
     const db = getDb();
     return db
       .collection("users")
-      .findOne({ _id: new mongodb.ObjectId(userId) })
+      .findOne({ _id: new ObjectId(userId) })
       .then((user) => {
         console.log(user);
         return user;
       })
       .catch((err) => console.log(err));
-    // .next()
-    // .then((user) => {
-    //   console.log(user);
-    //   return user;
-    // })
-    // .catch((err) => console.log(err));
   }
 }
 
