4bcb80350fb0c562371ee4e51337070e85e01325
Almir Muminovic <projectalmir@gmail.com>
2022-03-23 12:04:30 +0100

    Creating New Users

--- Learning-NodeJS/Learning-Database-NoSQL/app.js
+++ Learning-NodeJS/Learning-Database-NoSQL/app.js
@@ -1,40 +1,40 @@
-const dotEnv = require('dotenv')
-dotEnv.config()
-const path = require('path');
+const dotEnv = require("dotenv");
+dotEnv.config();
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
+const errorController = require("./controllers/error");
+const mongoConnect = require("./util/database").mongoConnect;
+const User = require("./models/user");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next()
+  User.findById("623afdf746864094f1facc12")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+  next();
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-
 mongoConnect(() => {
-  app.listen(3001)
-})
+  app.listen(3001);
+});
--- Learning-NodeJS/Learning-Database-NoSQL/models/user.js
+++ Learning-NodeJS/Learning-Database-NoSQL/models/user.js
@@ -1,16 +1,32 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
 
-const sequelize = require('../util/database');
+class User {
+  constructor(username, email, id) {
+    this.name = username;
+    this.email = email;
+    this._id = id ? new mongodb.ObjectId(id) : null;
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  save() {
+    const db = getDb();
+    return db.collection("users").insertOne(this);
+    // .then((result) => {
+    //   console.log(result);
+    // })
+    // .catch((err) => console.log(err));
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    db.collection("users").findOne({ _id: new mongodb.ObjectId(userId) });
+    // .next()
+    // .then((user) => {
+    //   console.log(user);
+    //   return user;
+    // })
+    // .catch((err) => console.log(err));
+  }
+}
 
 module.exports = User;
