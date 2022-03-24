57b401878e325336345c4e15d0cf08ba4eaea0c6
Almir Muminovic <projectalmir@gmail.com>
2022-03-24 12:32:02 +0100

    Adding the Add to Cart Functionality

--- Learning-NodeJS/Learning-Database-NoSQL/app.js
+++ Learning-NodeJS/Learning-Database-NoSQL/app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("623afdf746864094f1facc12")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
