3fc1345d1a0029a5921695f0612d9f62d2bb66c9
Almir Muminovic <projectalmir@gmail.com>
2022-03-30 14:07:42 +0200

    Fetching All Products

--- Learning-NodeJS/Learning-Database-NoSQL/controllers/shop.js
+++ Learning-NodeJS/Learning-Database-NoSQL/controllers/shop.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then((products) => {
       res.render("shop/product-list", {
         prods: products,
@@ -37,8 +37,9 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then((products) => {
+      console.log(products);
       res.render("shop/index", {
         prods: products,
         pageTitle: "Shop",
