3f516611176aafc81df3e866c721ddcfb0203415
Almir Muminovic <projectalmir@gmail.com>
2022-04-01 15:33:11 +0200

    Clearing a cart after ordering it

--- Learning-NodeJS/Learning-Database-NoSQL/models/user.js
+++ Learning-NodeJS/Learning-Database-NoSQL/models/user.js
@@ -87,6 +87,11 @@ userSchema.methods.getOrders = function () {
     .toArray();
 };
 
+userSchema.methods.clearCart = function () {
+  this.cart = { items: [] };
+  return this.save();
+};
+
 module.exports = mongoose.model("User", userSchema);
 
 // const mongodb = require("mongodb");
