5074c6d2c3e069e8523cd13b8159e3d7f261d07b
Almir Muminovic <projectalmir@gmail.com>
2022-04-04 17:50:59 +0200

    Initializing the Session Middleware

--- Learning-NodeJS/Sessions-and-Cookies/app.js
+++ Learning-NodeJS/Sessions-and-Cookies/app.js
@@ -5,6 +5,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
@@ -20,6 +21,13 @@ const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "Typically long string of characters",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use((req, res, next) => {
   User.findById("6245c54f49fe872086b19288")
