375d4498e7eca9991e46f49a1bbf429e301f62c3
Almir Muminovic <projectalmir@gmail.com>
2022-04-04 17:04:10 +0200

    Setting a Cookie

---
+++ Learning-NodeJS/Sessions-and-Cookies/controllers/app.js
@@ -0,0 +1,60 @@
+const dotEnv = require("dotenv");
+dotEnv.config();
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const errorController = require("./controllers/error");
+const User = require("./models/user");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findById("5bab316ce0a7c75f783cb8a8")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.ekxmb.mongodb.net/shop?retryWrites=true&w=majority`
+  )
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Max",
+          email: "max@test.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    app.listen(3003);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- Learning-NodeJS/Sessions-and-Cookies/controllers/auth.js
+++ Learning-NodeJS/Sessions-and-Cookies/controllers/auth.js
@@ -1,12 +1,15 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req.get("Cookie").split("=")[1];
+  console.log(isLoggedIn);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: req.isLoggedIn,
+    isAuthenticated: isLoggedIn,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  req.isLoggedIn = true;
+  //   req.isLoggedIn = true; // request is dead after sending a response
+  res.setHeader("Set-Cookie", "loggedIn=true");
   res.redirect("/");
 };
