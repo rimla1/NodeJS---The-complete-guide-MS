355c54c6c3b4d4e691130d1098a3c15297a1cc03
Almir Muminovic <projectalmir@gmail.com>
2022-04-05 13:23:59 +0200

    Using the Session Middleware

--- Learning-NodeJS/Sessions-and-Cookies/controllers/auth.js
+++ Learning-NodeJS/Sessions-and-Cookies/controllers/auth.js
@@ -1,15 +1,15 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get("Cookie").split("=")[1] === "true";
-  console.log(isLoggedIn);
+  //   const isLoggedIn = req.get("Cookie").split("=")[1] === "true";
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: req.session.isLoggedIn,
   });
 };
 
 exports.postLogin = (req, res, next) => {
   //   req.isLoggedIn = true; // request is dead after sending a response
-  res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly");
+  //   res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly"); // Using cookie
+  req.session.isLoggedIn = true;
   res.redirect("/");
 };
