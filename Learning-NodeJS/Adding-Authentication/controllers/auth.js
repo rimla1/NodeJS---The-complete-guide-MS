bf56fe09c266570335593182d4d3b6d8d148ea90
Almir Muminovic <projectalmir@gmail.com>
2022-04-13 17:03:27 +0200

    Basic setup + Implementing an authentication flow

---
+++ Learning-NodeJS/Adding-Authentication/controllers/auth.js
@@ -0,0 +1,80 @@
+const sequelize = require("sequelize");
+const User = require("../models/user");
+
+exports.getLogin = (req, res, next) => {
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: false,
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    isAuthenticated: false,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById("5bab316ce0a7c75f783cb8a8")
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save((err) => {
+        console.log(err);
+        res.redirect("/");
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+// exports.postSignup = (req, res, next) => {
+//   const email = req.body.email;
+//   const password = req.body.password;
+//   const confirmPassword = req.body.confirmPassword;
+//   User.findOne({ email: email })
+//     .then((userDoc) => {
+//       console.log(userDoc);
+//       if (userDoc) {
+//         return res.redirect("/signup");
+//       }
+//       const user = new User({
+//         email: email,
+//         password: password,
+//         cart: { items: [] },
+//       });
+//       return user.save();
+//     })
+//     .then((result) => res.redirect("/login"))
+//     .catch((err) => console.log(err));
+// };
+
+exports.postSignup = async (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  try {
+    const userDoc = await User.findOne({ email: email });
+    if (userDoc) {
+      return res.redirect("/signup");
+    }
+    const user = new User({
+      email: email,
+      password: password,
+      cart: { items: [] },
+    });
+    await user.save();
+    res.redirect("/login");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
+};
