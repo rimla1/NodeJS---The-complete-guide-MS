b3943cee75b31ebc9c4f6f7382bb914d38b6f865
Almir Muminovic <projectalmir@gmail.com>
2022-04-14 01:16:02 +0200

    Adding sign in funtionality

--- Learning-NodeJS/Adding-Authentication/controllers/auth.js
+++ Learning-NodeJS/Adding-Authentication/controllers/auth.js
@@ -1,4 +1,4 @@
-const bcrypte = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 
 const User = require("../models/user");
 
@@ -18,17 +18,39 @@ exports.getSignup = (req, res, next) => {
   });
 };
 
-exports.postLogin = (req, res, next) => {
-  User.findById("5bab316ce0a7c75f783cb8a8")
-    .then((user) => {
+// exports.postLogin = (req, res, next) => {
+//   User.findById("5bab316ce0a7c75f783cb8a8")
+//     .then((user) => {
+//       req.session.isLoggedIn = true;
+//       req.session.user = user;
+//       req.session.save((err) => {
+//         console.log(err);
+//         res.redirect("/");
+//       });
+//     })
+//     .catch((err) => console.log(err));
+// };
+
+exports.postLogin = async (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.redirect("/login");
+    }
+    const doesPasswordsMatch = await bcrypt.compare(password, user.password);
+    if (doesPasswordsMatch) {
       req.session.isLoggedIn = true;
       req.session.user = user;
-      req.session.save((err) => {
-        console.log(err);
+      return req.session.save((err) => {
         res.redirect("/");
       });
-    })
-    .catch((err) => console.log(err));
+    }
+    res.redirect("/login");
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 // exports.postSignup = (req, res, next) => {
@@ -61,7 +83,7 @@ exports.postSignup = async (req, res, next) => {
     if (userDoc) {
       return res.redirect("/signup");
     }
-    const hashedPassword = await bcrypte.hash(password, 12);
+    const hashedPassword = await bcrypt.hash(password, 12);
     const user = new User({
       email: email,
       password: hashedPassword,
@@ -76,7 +98,6 @@ exports.postSignup = async (req, res, next) => {
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
-    console.log(err);
     res.redirect("/");
   });
 };
