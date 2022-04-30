be9d8f47110def458c7a04ea1f3a70516d5482c4
Almir Muminovic <projectalmir@gmail.com>
2022-04-30 14:50:03 +0200

    Using the Express.js error handling middleware

--- Learning-NodeJS/Adding-Authentication/controllers/admin.js
+++ Learning-NodeJS/Adding-Authentication/controllers/admin.js
@@ -53,7 +53,9 @@ exports.postAddProduct = (req, res, next) => {
       res.redirect("/admin/products");
     })
     .catch((err) => {
-      res.redirect("/500");
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -78,7 +80,11 @@ exports.getEditProduct = (req, res, next) => {
         validationErrors: [],
       });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.postEditProduct = (req, res, next) => {
