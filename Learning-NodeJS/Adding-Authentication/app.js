f72df1c4107da81056d3032478933dfe89f2c7e0
Almir Muminovic <projectalmir@gmail.com>
2022-05-03 17:28:07 +0200

    Using the Error Handling Middleware Correctly

--- Learning-NodeJS/Adding-Authentication/app.js
+++ Learning-NodeJS/Adding-Authentication/app.js
@@ -44,11 +44,18 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
   }
   User.findById(req.session.user._id)
     .then((user) => {
+      throw new Error("Dummy Error");
       if (!user) {
         return next();
       }
@@ -56,14 +63,8 @@ app.use((req, res, next) => {
       next();
     })
     .catch((err) => {
-      throw new Error(err);
+      next(new Error(err));
     });
-});
-
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
 });
 
 app.use("/admin", adminRoutes);
@@ -75,7 +76,11 @@ app.get("/500", errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  res.redirect("/500");
+  res.status(500).render("500", {
+    pageTitle: "Server error",
+    path: "/404",
+    isAuthenticated: req.session.isLoggedIn,
+  });
 });
 
 mongoose
