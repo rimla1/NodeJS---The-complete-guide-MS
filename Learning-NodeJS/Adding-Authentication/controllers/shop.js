c511d974fce926f1f53af8f0bbaa8a3df24c93f2
Almir Muminovic <projectalmir@gmail.com>
2022-05-11 14:35:33 +0200

    Re-Using the Pagination Logic & Controls

--- Learning-NodeJS/Adding-Authentication/controllers/shop.js
+++ Learning-NodeJS/Adding-Authentication/controllers/shop.js
@@ -8,13 +8,29 @@ const Order = require("../models/order");
 const ITEMS_PER_PAGE = 2;
 
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then((numProducts) => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(2);
+    })
     .then((products) => {
       console.log(products);
       res.render("shop/product-list", {
         prods: products,
         pageTitle: "All Products",
         path: "/products",
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch((err) => {
@@ -43,7 +59,6 @@ exports.getProduct = (req, res, next) => {
 
 exports.getIndex = (req, res, next) => {
   const page = +req.query.page || 1;
-  console.log(page);
   let totalItems;
 
   Product.find()
