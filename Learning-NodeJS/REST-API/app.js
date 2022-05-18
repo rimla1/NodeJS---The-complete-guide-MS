4ee38e11fe09cf7f8ce0babfe2c1141a672ecd43
Almir Muminovic <projectalmir@gmail.com>
2022-05-18 13:11:33 +0200

    REST APIs, Clients & CORS errors

--- Learning-NodeJS/REST-API/app.js
+++ Learning-NodeJS/REST-API/app.js
@@ -9,6 +9,25 @@ const app = express();
 
 app.use(bodyParser.json()); // application/json
 
+// Solving CORS error
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Every website is allowed
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE"
+  ); // Methods that we can use
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type",
+    "Authorization"
+  );
+  next();
+});
+
 app.use("/feed", feedRoutes);
 
 app.listen(8080);
