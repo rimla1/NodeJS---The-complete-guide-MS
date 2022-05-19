99a4315a8f7086ff745d313ce18c706b3e59a759
Almir Muminovic <projectalmir@gmail.com>
2022-05-19 16:39:15 +0200

    Solve bug that we have because on async code while throwing error in async world

--- Learning-NodeJS/REST-API/controllers/feed.js
+++ Learning-NodeJS/REST-API/controllers/feed.js
@@ -18,34 +18,68 @@ exports.getPosts = (req, res, next) => {
   });
 };
 
-exports.createPost = async (req, res, next) => {
+// exports.createPost = async (req, res, next) => {
+//   const errors = validationResult(req);
+//   if (!errors.isEmpty()) {
+//     const error = new Error("Validation failed, entered data is incorrect");
+//     error.statusCode = 422;
+//     next(error);
+//   }
+//   const title = req.body.title;
+//   const content = req.body.content;
+//   try {
+//     const post = new Post({
+//       title: title,
+//       imageUrl: "images/city.jpeg",
+//       content: content,
+//       creator: {
+//         name: "Rimla",
+//       },
+//     });
+//     await post.save();
+//     console.log(post),
+//       res.status(201).json({
+//         message: "Post created successfully",
+//         post: post,
+//       });
+//   } catch (err) {
+//     if (!err.statusCode) {
+//       err.statusCode = 500;
+//     }
+//     next(err);
+//   }
+// };
+
+exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = new Error("Validation failed, entered data is incorrect");
     error.statusCode = 422;
-    next(error);
+    throw error;
   }
-  const title = req.body.title;
-  const content = req.body.content;
-  try {
-    const post = new Post({
-      title: title,
-      imageUrl: "images/city.jpeg",
-      content: content,
-      creator: {
-        name: "Rimla",
-      },
-    });
-    await post.save();
-    console.log(post),
+
+  const post = new Post({
+    title: title,
+    imageUrl: "images/city.jpeg",
+    content: content,
+    creator: {
+      name: "Rimla",
+    },
+  });
+
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
       res.status(201).json({
-        message: "Post created successfully",
-        post: post,
+        message: "Post created successfully!",
+        post: result,
       });
-  } catch (err) {
-    if (!err.statusCode) {
-      err.statusCode = 500;
-    }
-    next(err);
-  }
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
