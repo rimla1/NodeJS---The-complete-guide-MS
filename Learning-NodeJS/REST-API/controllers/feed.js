d6e7c24f0c8475920484e42d5067379d55cf4938
Almir Muminovic <projectalmir@gmail.com>
2022-05-20 16:00:23 +0200

    Fetchinga posts from database

--- Learning-NodeJS/REST-API/controllers/feed.js
+++ Learning-NodeJS/REST-API/controllers/feed.js
@@ -2,21 +2,21 @@ const { validationResult } = require("express-validator");
 const post = require("../models/post");
 const Post = require("../models/post");
 
-exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: "1",
-        title: "First post",
-        content: "This is the first post!",
-        imageUrl: "images/city.jpeg",
-        creator: {
-          name: "Almir",
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+exports.getPosts = async (req, res, next) => {
+  try {
+    const posts = await Post.find();
+    // if (!posts) {
+    //   res.status(200).json({ message: "No posts found!" });
+    // }
+    res
+      .status(200)
+      .json({ message: "Posts fetched successfully!", posts: posts });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
 // exports.createPost = async (req, res, next) => {
@@ -53,6 +53,8 @@ exports.getPosts = (req, res, next) => {
 
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
+  const title = req.body.title;
+  const content = req.body.content;
   if (!errors.isEmpty()) {
     const error = new Error("Validation failed, entered data is incorrect");
     error.statusCode = 422;
--- Learning-NodeJS/REST-API/routes/feed.js
+++ Learning-NodeJS/REST-API/routes/feed.js
@@ -13,7 +13,7 @@ router.get("/posts", feedController.getPosts);
 router.post(
   "/post",
   [
-    body("title").trim().isLength({ min: 7 }),
+    body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
   ],
   feedController.createPost
