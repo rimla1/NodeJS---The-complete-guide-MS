f46d3e56ced12078efcea84b366bf77c256cb3cd
Almir Muminovic <projectalmir@gmail.com>
2022-05-21 19:54:57 +0200

    Adding User Signup Validation

--- Learning-NodeJS/REST-API/routes/auth.js
+++ Learning-NodeJS/REST-API/routes/auth.js
@@ -1,7 +1,28 @@
 const express = require("express");
+const { body } = require("express-validator");
+const authController = require("../controllers/auth");
+
+const User = require("../models/user");
 
 const router = express.Router();
 
-router.put("/signup");
+router.put(
+  "/signup",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .custom(async (value, { req }) => {
+        const userDoc = await User.findOne({ email: value });
+        if (userDoc) {
+          return Promise.reject("E-mail address already exists!");
+        }
+      })
+      .normalizeEmail(),
+    body("password").trim().isLength({ min: 5 }),
+    body("name").trim().not().isEmpty(),
+  ],
+  authController.signup
+);
 
 module.exports = router;
