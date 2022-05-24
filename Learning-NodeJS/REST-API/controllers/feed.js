ac4d788b9dae5bd104ff392546ab8ff27bfdc231
Almir Muminovic <projectalmir@gmail.com>
2022-05-24 23:58:43 +0200

    Connecting posts & users

--- Learning-NodeJS/REST-API/controllers/feed.js
+++ Learning-NodeJS/REST-API/controllers/feed.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const path = require("path");
 
 const { validationResult } = require("express-validator");
-const post = require("../models/post");
 const Post = require("../models/post");
+const User = require("../models/user");
 
 exports.getPosts = async (req, res, next) => {
   const currentPage = req.query.page || 1;
@@ -76,22 +76,29 @@ exports.createPost = (req, res, next) => {
   const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
+  let creator;
   const post = new Post({
     title: title,
     imageUrl: imageUrl,
     content: content,
-    creator: {
-      name: "Rimla",
-    },
+    creator: req.userId,
   });
 
   post
     .save()
     .then((result) => {
-      console.log(result);
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      creator = user;
+      user.posts.push(post);
+      return user.save();
+    })
+    .then((result) => {
       res.status(201).json({
         message: "Post created successfully!",
-        post: result,
+        post: post,
+        creator: { _id: creator._id, name: creator.name },
       });
     })
     .catch((err) => {
