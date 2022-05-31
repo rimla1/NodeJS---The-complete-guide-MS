4c87d8c8a8b19b0506cf9b9e99d8474c99bb126d
Almir Muminovic <projectalmir@gmail.com>
2022-05-31 16:52:05 +0200

    Setting Socket.io on the server

--- Learning-NodeJS/REST-API/app.js
+++ Learning-NodeJS/REST-API/app.js
@@ -78,6 +78,11 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then((result) => {
-    app.listen(8080), console.log("Connected to the database");
+    const server = app.listen(8080);
+    console.log("Connected to the database");
+    const io = require("socket.io")(server);
+    io.on("connection", (socket) => {
+      console.log("Client connected");
+    });
   })
   .catch((err) => console.log(err));
