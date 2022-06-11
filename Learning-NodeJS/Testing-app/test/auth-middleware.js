0afe93762dec96af779a08c1f56bd84a4e49ce24
Almir Muminovic <projectalmir@gmail.com>
2022-06-11 19:09:33 +0200

    Organizing multiple test

--- Learning-NodeJS/Testing-app/test/auth-middleware.js
+++ Learning-NodeJS/Testing-app/test/auth-middleware.js
@@ -2,14 +2,26 @@ const expect = require("chai").expect;
 
 const authMiddleware = require("../middleware/is-auth");
 
-it("should throw an error if no autorization header is present", () => {
-  const req = {
-    get: (headerName) => {
-      return null;
-    },
-  };
+describe("Auth middleware", () => {
+  it("should throw an error if no autorization header is present", () => {
+    const req = {
+      get: (headerName) => {
+        return null;
+      },
+    };
 
-  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
-    "Not authenticated."
-  );
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      "Not authenticated."
+    );
+  });
+
+  it("should throw an error if no autorization header is only one string", () => {
+    const req = {
+      get: (headerName) => {
+        return "xzy";
+      },
+    };
+
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
 });
