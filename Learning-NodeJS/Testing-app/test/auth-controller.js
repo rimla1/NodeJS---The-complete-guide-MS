aa049fa42273d3aefe6b5069c5e3eb09f70e97e5
Almir Muminovic <projectalmir@gmail.com>
2022-06-12 19:16:05 +0200

    Testing Asynchronous code

--- Learning-NodeJS/Testing-app/test/auth-controller.js
+++ Learning-NodeJS/Testing-app/test/auth-controller.js
@@ -5,11 +5,21 @@ const User = require("../models/user");
 const AuthController = require("../controllers/auth");
 
 describe("Auth Controller - Login", () => {
-  it("It should throw an error with code 500 if there is no accessing to the db", () => {
+  it("It should throw an error with code 500 if there is no accessing to the db", (done) => {
     sinon.stub(User, "findOne");
     User.findOne.throws();
 
-    expect(AuthController.login);
+    const req = {
+      body: {
+        email: "userThatDoesNotExist@gmail.com",
+        password: "userThatDoesNotExist",
+      },
+    };
+    AuthController.login(req, {}, () => {}).then((result) => {
+      expect(result).to.be.an("error");
+      expect(result).to.have.property("statusCode", 500);
+      done();
+    });
 
     User.findOne.restore();
   });
