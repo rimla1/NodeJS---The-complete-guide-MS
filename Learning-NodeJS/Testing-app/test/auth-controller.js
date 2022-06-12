b7fb444bbca0649d281dc43adc1a41cb05d5f4f8
Almir Muminovic <projectalmir@gmail.com>
2022-06-12 20:51:04 +0200

    Setting up a Testing Database

--- Learning-NodeJS/Testing-app/test/auth-controller.js
+++ Learning-NodeJS/Testing-app/test/auth-controller.js
@@ -1,11 +1,14 @@
+const dotEnv = require("dotenv");
+dotEnv.config();
 const expect = require("chai").expect;
 const sinon = require("sinon");
+const mongoose = require("mongoose");
 
 const User = require("../models/user");
 const AuthController = require("../controllers/auth");
 
 describe("Auth Controller - Login", () => {
-  it("It should throw an error with code 500 if there is no accessing to the db", (done) => {
+  it("should throw an error with code 500 if there is no accessing to the db", (done) => {
     sinon.stub(User, "findOne");
     User.findOne.throws();
 
@@ -22,5 +25,23 @@ describe("Auth Controller - Login", () => {
     });
 
     User.findOne.restore();
+  });
+
+  it("should send a response with a valid user status for an existing user", (done) => {
+    mongoose
+      .connect(
+        `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.ekxmb.mongodb.net/${process.env.MONGODB_DB_NAME_TEST}?retryWrites=true&w=majority`
+      )
+      .then((result) => {
+        const user = new User({
+          email: "test10@test.com",
+          password: "tester",
+          name: "Test",
+          posts: [],
+        });
+        return user.save();
+      })
+      .then(() => {})
+      .catch((err) => console.log(err));
   });
 });
