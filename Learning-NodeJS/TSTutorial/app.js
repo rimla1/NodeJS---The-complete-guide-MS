f76eb4701cc694a4be384f67cac660a936ad410d
Almir Muminovic <projectalmir@gmail.com>
2022-06-16 14:08:49 +0200

    Type Inference & Type Casting

--- Learning-NodeJS/TSTutorial/app.js
+++ Learning-NodeJS/TSTutorial/app.js
@@ -1,5 +1,12 @@
-function add(num1, num2) {
-  return num1 + num2;
-}
-console.log(add(1, 6));
-console.log(add("1", "6"));
+var num1Element = document.getElementById("num1");
+var num2Element = document.getElementById("num2");
+var buttonElement = document.querySelector('button');
+var sum = function (num1, num2) {
+    return num1 + num2;
+};
+buttonElement.addEventListener("click", function () {
+    var num1 = num1Element.value;
+    var num2 = num2Element.value;
+    var result = sum(+num1, +num2);
+    console.log(result);
+});
