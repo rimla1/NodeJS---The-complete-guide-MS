bf6fdc9a0cc7072aa86d6de10c642e808434bb04
Almir Muminovic <projectalmir@gmail.com>
2022-06-16 16:01:41 +0200

    Using Object & Array Types

--- Learning-NodeJS/TSTutorial/app.js
+++ Learning-NodeJS/TSTutorial/app.js
@@ -2,6 +2,8 @@
 const num1Element = document.getElementById("num1");
 const num2Element = document.getElementById("num2");
 const buttonElement = document.querySelector('button');
+const numResults = [];
+const textResults = [];
 const sum = (num1, num2) => {
     if (typeof num1 === "number" && typeof num2 === "number") {
         return num1 + num2;
@@ -11,11 +13,16 @@ const sum = (num1, num2) => {
     }
     return +num1 + +num2;
 };
+const printResult = (resultObj) => {
+    console.log(resultObj.val);
+};
 buttonElement.addEventListener("click", () => {
     const num1 = num1Element.value;
     const num2 = num2Element.value;
     const result = sum(+num1, +num2);
+    numResults.push(result);
     const stringResult = sum(num1, num2);
-    console.log(result);
-    console.log(stringResult);
+    textResults.push(stringResult);
+    printResult({ val: result, timestamp: new Date() });
+    console.log(numResults, stringResult);
 });
--- Learning-NodeJS/TSTutorial/app.ts
+++ Learning-NodeJS/TSTutorial/app.ts
@@ -2,6 +2,9 @@ const num1Element = document.getElementById("num1") as HTMLInputElement;
 const num2Element = document.getElementById("num2") as HTMLInputElement;
 const buttonElement = document.querySelector('button')!
 
+const numResults: number[] = []
+const textResults: string[] = []
+
 const sum = (num1: number | string, num2: number | string) => {
   if(typeof num1 === "number" && typeof num2 === "number") {
     return num1 + num2
@@ -12,11 +15,17 @@ const sum = (num1: number | string, num2: number | string) => {
   return +num1 + +num2
 }
 
+const printResult = (resultObj: {val: number, timestamp: Date}) => {
+  console.log(resultObj.val)
+}
+
 buttonElement.addEventListener("click", () => {
   const num1 = num1Element.value;
   const num2 = num2Element.value;
   const result = sum(+num1, +num2)
+  numResults.push(result as number)
   const stringResult = sum(num1, num2)
-  console.log(result)
-  console.log(stringResult)
+  textResults.push(stringResult as string)
+  printResult({val: result as number, timestamp: new Date()})
+  console.log(numResults, stringResult)
 })
