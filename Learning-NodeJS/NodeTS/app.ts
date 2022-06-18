cb9a1c04976641a38bae13d6c9c95f292d66ca8b
Almir Muminovic <projectalmir@gmail.com>
2022-06-18 18:53:39 +0200

    Adding REST Routes with TypeScript

--- Learning-NodeJS/NodeTS/app.ts
+++ Learning-NodeJS/NodeTS/app.ts
@@ -1,7 +1,11 @@
 import express from "express"
+import bodyParser from "body-parser";
+
 import todoRoutes from "./routes/todos"
 
 const app = express();
+
+app.use(bodyParser.json())
 
 app.use(todoRoutes)
 
